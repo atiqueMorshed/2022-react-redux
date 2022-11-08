72ee0a1f9d2f4acd174193e4176ce4da9fe6f0b4
Atique Morshed <iamsamix17@gmail.com>
2022-11-08 15:39:51 +0600

    Adds frontend for simple todo.

---
+++ src/components/Header.tsx
@@ -0,0 +1,40 @@
+import tickImage from '../assets/images/double-tick.png';
+import noteImage from '../assets/images/notes.png';
+export default function Header() {
+  return (
+    <div>
+      <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
+        <img src={noteImage} className="w-6 h-6" alt="Add todo" />
+        <input
+          type="text"
+          placeholder="Type your todo"
+          className="w-full text-lg px-4 py-1 bg-plusIcon border-none outline-none bg-gray-100 text-gray-500"
+        />
+        <button type="submit">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </button>
+      </form>
+
+      <ul className="flex justify-between my-4 text-xs text-gray-500">
+        <li className="flex space-x-1 cursor-pointer">
+          <img className="w-4 h-4" src={tickImage} alt="Complete" />
+          <span>Complete All Tasks</span>
+        </li>
+        <li className="cursor-pointer">Clear completed</li>
+      </ul>
+    </div>
+  );
+}
